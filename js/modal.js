8cc7c27401261eb88b00aa3c2a7cfc7bf0d5ca94
Vasia911119 <vasia911119@gmail.com>
2023-11-02 17:34:55 +0200

    add modal code

--- js/modal.js
+++ js/modal.js
@@ -5,10 +5,37 @@
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", openModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  const classList = refs.modal.classList;
+
+  // Closing the modal when the Escape key is pressed
+  function handleEscapeKey(e) {
+    if (e.key === "Escape") {
+      closeModal();
+    }
+  }
+
+  // Closing the modal when clicking outside of it
+  function handleClickOutside(e) {
+    if (e.target === refs.modal) {
+      closeModal();
+    }
+  }
+
+  function openModal() {
+    classList.remove("is-hidden");
+    refs.openModalBtn.removeEventListener("click", openModal);
+    refs.closeModalBtn.addEventListener("click", closeModal);
+    document.addEventListener("keydown", handleEscapeKey);
+    document.addEventListener("click", handleClickOutside);
+  }
+
+  function closeModal() {
+    classList.add("is-hidden");
+    refs.openModalBtn.addEventListener("click", openModal);
+    refs.closeModalBtn.removeEventListener("click", closeModal);
+    document.removeEventListener("keydown", handleEscapeKey);
+    document.removeEventListener("click", handleClickOutside);
   }
 })();
