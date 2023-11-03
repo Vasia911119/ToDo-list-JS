2af469c38c7d4c5251a8a548f7432f8c989d4071
Vasia911119 <vasia911119@gmail.com>
2023-11-03 20:33:27 +0200

    refactoring code and add some functionality

--- js/loadData.js
+++ js/loadData.js
@@ -1,33 +1,36 @@
-fetch("../data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const categorySelect = document.querySelector('select[name="category"]');
-    const timeSelect = document.querySelector('select[name="time"]');
+(() =>
+  fetch("../data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const categorySelect = document.querySelector('select[name="category"]');
+      const timeSelect = document.querySelector('select[name="time"]');
 
-    data.categories.forEach((category) => {
-      const option = document.createElement("option");
-      option.value = category.value;
-      option.textContent = category.name;
+      data.categories.forEach((category) => {
+        const option = document.createElement("option");
+        option.value = category.value;
+        option.textContent = category.name;
 
-      if (category.value === "initial") {
-        option.setAttribute("disabled", true);
-        option.setAttribute("selected", true);
-      }
+        if (category.value === "initial") {
+          option.setAttribute("disabled", true);
+          option.setAttribute("selected", true);
+        }
 
-      categorySelect.appendChild(option);
-    });
+        categorySelect.appendChild(option);
+      });
 
-    data.times.forEach((time) => {
-      const option = document.createElement("option");
-      option.value = time.value;
-      option.textContent = time.name;
+      data.times.forEach((time) => {
+        const option = document.createElement("option");
+        option.value = time.value;
+        option.textContent = time.name;
 
-      if (time.value === "initial") {
-        option.setAttribute("disabled", true);
-        option.setAttribute("selected", true);
-      }
+        if (time.value === "initial") {
+          option.setAttribute("disabled", true);
+          option.setAttribute("selected", true);
+        }
 
-      timeSelect.appendChild(option);
-    });
-  })
-  .catch((error) => console.error("Помилка завантаження файлу JSON:", error));
+        timeSelect.appendChild(option);
+      });
+    })
+    .catch((error) =>
+      console.error("Помилка завантаження файлу JSON:", error)
+    ))();
